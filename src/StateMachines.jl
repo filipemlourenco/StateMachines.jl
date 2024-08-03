8c6f75c20748d27fc2ea68bd14c9b252af0b6c23
filipemlourenco <filipe.lourenco.gmail.com>
2024-08-03 23:05:51 +0100

    Initial development

---
+++ src/StateMachines.jl
@@ -0,0 +1,88 @@
+module StateMachines
+
+export State
+export Transition
+export Automaton
+
+MULTISTEP = true
+
+# --- Models
+const State = String
+
+struct Transition
+    from        :: State
+    to          :: State
+    input       :: Union{Nothing, Symbol, Function}
+end
+Transition(from::State, to::State) = Transition(from, to, nothing)
+
+mutable struct Automaton
+    states      :: Vector{State}
+    transitions :: Vector{Transition}
+    start       :: Union{Nothing, State}
+
+    state     :: State
+
+    function Automaton(states::Vector, transitions::Vector, start::Union{Nothing, State} = nothing)
+        @assert length(states) > 1 "Automaton should have at least two elements"
+        @assert !isempty(transitions) "Automaton should have at least one transition"
+
+        start = isnothing(start) ? states[1] : start
+        @assert start in states "'$start' state not present in the states list"
+
+        return new(states, transitions, start, start)
+    end
+
+    function Automaton(transitions::Vector, start::Union{Nothing, State} = nothing)
+        states = unique([map(x -> x.from, transitions)..., map(x -> x.to, transitions)...])
+        return Automaton(states, transitions, start)
+    end
+end
+
+# --- Getters
+states(w::Automaton)    = w.states
+start(w::Automaton)     = w.start
+state(w::Automaton)     = w.state
+current(w::Automaton)   = w.state
+transitions(w::Automaton) :: Vector = [(k => n.to) for (k,v) in w.transitions, n in v]
+transitions(w::Automaton, s::State) :: Vector = [t.to for t in w[s].transitions]
+
+# --- Setters
+multistep!()    = (global MULTISTEP = true)
+singlestep!()   = (global MULTISTEP = false)
+
+# --- Automaton (state machine) execution
+function exec!(w::Automaton, action::Union{Symbol, Nothing} = nothing, context::Any = nothing)
+    w.state = exec(w, w.state, action, context)
+end
+
+exec(w::Automaton, context::Any = nothing)                  = exec(w, w.state, nothing, context)
+exec(w::Automaton, action::Symbol, context::Any = nothing)  = exec(w, w.state, action,  context)
+function exec(w::Automaton, s::State, action::Union{Symbol, Nothing}, context::Any = nothing) :: State
+    @assert s in w.states "State not available in the automaton ($s)"
+
+    prev = s
+    n = _exec(w, prev, action, context)
+    if MULTISTEP
+        while(prev != n)
+            prev = n
+            n = _exec(w, prev, nothing, context)
+        end
+    end
+
+    return n
+end
+
+function _exec(w::Automaton, s::State, action::Union{Symbol, Nothing}, context::Any = nothing) :: State
+    trans = filter(x -> x.from == s, w.transitions) 
+    for t in trans
+        if isnothing(t.input) ||
+            (t.input isa Symbol && t.input == action) ||
+            (t.input isa Function && t.input(action, context))
+                return t.to
+        end
+    end
+    return s
+end
+
+end # module StateMachines
---
+++ test/runtests.jl
@@ -0,0 +1,123 @@
+using StateMachines
+using Test
+
+@testset "Object instantiation" begin
+
+    @test State("test") == "test"
+
+    @test Transition(State("from"), State("to")).from == "from"
+    @test Transition(State("from"), State("to")).to == "to"
+    @test Transition(State("from"), State("to")).input === nothing
+    @test Transition(State("from"), State("to"), :write).input == :write
+
+    a = Automaton(
+        [State("1"), State("2"), State("3"), State("4"), State("5"), State("6")],
+        [
+            Transition(State("1"), State("2"))
+            Transition(State("2"), State("3"))
+            Transition(State("3"), State("4"))
+        ],
+        State("2")
+    )
+
+    @test a.start == "2"
+    @test a.state == "2"
+    @test length(a.transitions) == 3
+    @test a.states == ["1", "2", "3", "4", "5", "6"]
+
+    b = Automaton([
+        Transition(State("1"), State("2"))
+        Transition(State("2"), State("3"))
+    ])
+
+    @test b.start == "1"
+    @test length(b.transitions) == 2
+    @test b.states == ["1", "2", "3"]
+
+end
+
+@testset "State Machine" begin
+
+    a1 = Automaton([
+        Transition(State("1"), State("2"))
+        Transition(State("2"), State("3"))
+    ])
+    @test StateMachines.exec(a1) == "3"
+    @test StateMachines.exec(a1, State("2")) == "3"
+
+    a2 = Automaton([
+        Transition(State("1"), State("2"))
+        Transition(State("2"), State("3"))
+        Transition(State("3"), State("4"), :update)
+    ])
+    @test StateMachines.exec(a2) == "3"
+    @test StateMachines.exec(a2, :update) == "3"
+    @test StateMachines.exec(a2, State("3"), :update) == "4"
+
+    a3 = Automaton([
+        Transition(State("1"), State("2"), :create)
+        Transition(State("2"), State("3"))
+        Transition(State("1"), State("4"), :update)
+    ])
+    @test StateMachines.exec(a3, :create) == "3"
+    @test StateMachines.exec(a3, :update) == "4"
+
+    a4 = Automaton([
+        Transition(State("1"), State("2"), (a,_) -> a == :create)
+        Transition(State("2"), State("3"))
+        Transition(State("1"), State("4"), (a,_) -> a == :update)
+    ])
+    @test StateMachines.exec(a4, :create) == "3"
+    @test StateMachines.exec(a4, :update) == "4"
+
+    a4.state = "1"
+    StateMachines.exec!(a4, :create)
+    @test a4.state == "3"
+
+end
+
+@testset "State Machine (inner state)" begin
+
+    a1 = Automaton([
+        Transition(State("1"), State("2"), (a,_) -> a == :create)
+        Transition(State("2"), State("3"))
+        Transition(State("1"), State("4"), (a,_) -> a == :update)
+    ])
+    a1.state = "1"
+    StateMachines.exec!(a1, :create)
+    @test a1.state == "3"
+
+    a1.state = "1"
+    StateMachines.exec!(a1, :update)
+    @test a1.state == "4"
+
+end
+
+@testset "State Machine (with context)" begin
+
+    a1 = Automaton([
+        Transition(State("1"), State("2"), (a,c) -> a == :prepare && c == "valid")
+        Transition(State("2"), State("3"), (a,c) -> a == :review && c == "prepared")
+        Transition(State("1"), State("4"), (a,c) -> a == :delete && c != "reviewed")
+    ])
+    a1.state = "1"
+    StateMachines.exec!(a1, :prepare, "invalid")
+    @test a1.state == "1"
+
+    a1.state = "1"
+    StateMachines.exec!(a1, :prepare, "valid")
+    @test a1.state == "2"
+
+    a1.state = "2"
+    StateMachines.exec!(a1, :review, "prepared")
+    @test a1.state == "3"
+
+    a1.state = "1"
+    StateMachines.exec!(a1, :delete, "prepared")
+    @test a1.state == "4"
+
+    a1.state = "1"
+    StateMachines.exec!(a1, :delete, "reviewed")
+    @test a1.state == "1"
+
+end
